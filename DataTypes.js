339851d7c0d2e4a8fc6bd54ed7458046b1e7683d
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-08-12 21:23:57 +0530

    Numbers and strings added

---
+++ DataTypes.js
@@ -0,0 +1,22 @@
+/* Basic Understanding of variables in JS */
+
+/* Declaring variables using var */
+var age = 32, first_name = 'Jack';
+console.log("First name is :: "+first_name+ " of age :: "+age)
+
+/* String with double quotes */
+var string_option_01 = "Jack's car";
+var string_option_02 = "Jack's car name is \"Tesla\" which is nice.";
+console.log(string_option_02)
+
+/* Property associated with strings */
+console.log(string_option_02.constructor, string_option_02.length)
+
+/* Numbers */
+var number = 12;
+let x = 12;
+
+/* Boolean */
+var isUrlValid = true
+
+/* Objects */
