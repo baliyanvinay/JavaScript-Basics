db2b8b17c104e4af3f697e86ccbf69a1ba654e3c
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-08-17 17:01:18 +0530

    While loop added

--- Loops.js
+++ Loops.js
@@ -3,3 +3,15 @@
 for (let i=1; i<10; i++) {
     console.log('*'.repeat(i))
 }
+// Using in keyword to loop
+const weekDays = ['Sunday','Monday','Tuesday','Wednessday','Thursday','Friday','Saturday']
+for (let day_index in weekDays) {
+    console.log(day_index, weekDays[day_index])
+}
+
+/* While loop */
+let x = 5;
+while (x>0) {
+    console.log('*'.repeat(x));
+    x--;
+}
