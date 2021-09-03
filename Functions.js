fb819d065ee6e4f13b20130cc7d4277a867542e7
Vinay Baliyan <baliyanvinay17@outlook.com>
2021-09-03 14:40:16 +0530

    Functions with objects

--- Functions.js
+++ Functions.js
@@ -15,3 +15,34 @@ function helloWorld(text='Hello Someone!') {
     return text
 }
 console.log(helloWorld('Hello Sam!'))
+
+/* Objects and functions */
+
+// Objects are passed by reference
+var car = {
+    "Name": "Kia",
+    "Model": "Seltos",
+    "Year": 2016
+};
+
+function change_year(vehicle) {
+    vehicle.Year = 2012;
+};
+
+change_year(car);
+console.log(car);
+
+// Changing by value
+
+function change_year_val(vehicle) {
+    year = vehicle['Year'];
+    year = 2020;
+    return {
+        Name: vehicle.Name,
+        Model: vehicle.Model,
+        Year: year
+    };
+};
+
+new_car = change_year_val(car);
+console.log("Car is ",car,"|| New car is ",new_car);
